57a3fbe27c988ff524268fb507722215c18abf7a
botshen <363088847@qq.com>
2020-12-02 12:05:35 +0800

    compents

--- src/main.js
+++ src/main.js
@@ -1,24 +1,32 @@
-console.log(window.vue)
 
 const Vue = window.Vue
+
 
 Vue.config.productionTip = false
 
 new Vue({
     data() {
         return {
-            n: 0
+            n: 0,
+            array: [1,2,3,4,5,6,7,8]
         }
     },
     template: `
       <div class="red">
-      {{ n }}
+      {{n}}
       <button @click="add">+1</button>
       </div>
     `,
+    created() {
+        console.log("我还没出现在页面中")
+    },
+    mounted() {
+        debugger
+        console.log("我已经出现在页面中")
+    },
     methods: {
         add() {
             this.n += 1
-        }
-    }
+        },
+},
 }).$mount('#frank')
